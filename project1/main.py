b63034d0c859fccab2a735225fddd886e1336d5b
Yusuf Ulgen <ysfulgen142@gmail.com>
2025-04-11 15:17:39 +0300

    In this application, we have listed the results according to the entered word. You can reach the relevant site by clicking on the URLs of the results.

---
+++ project1/main.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Loads .env file
+
+API_KEY = os.getenv("API_KEY")
+CX = os.getenv("CX")
+
+def google_search(word):
+    url = "https://www.googleapis.com/customsearch/v1"
+    params = {
+        "key": API_KEY,
+        "cx": CX,
+        "q": word
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    for i, item in enumerate(data.get("items", []), 1):
+        print(f"{i}. Title: {item['title']}")
+        print(f"   Link: {item['link']}")
+        print(f"   Explanation: {item.get('snippet', '')}\n")
+
+# Usage :
+google_search("Data Science")
